899e954c7256621bc2c4d3a6155c40e3356e2acf
Spider-Vice <spidervice@gmail.com>
2024-12-25 00:37:32 +0000

    feat: add cleanup when stopping/restarting the resource (#1)
    * feat: add cleanup when stopping/restarting the resource
    
    * fix: properly handle removing the tree
    
    * fix: correctly set rain level to the level of the current weather instead

--- client.js
+++ client.js
@@ -1,4 +1,7 @@
+const xmasTreeHash = GetHashKey('prop_xmas_ext');
 const xmasTreeOrigin = [240.82095336914062, -880.853515625, 28.792084503173828];
+
+let treeEntity;
 
 on('onClientResourceStart', (resource) => {
     if (resource === 'snow') {
@@ -6,7 +9,7 @@ on('onClientResourceStart', (resource) => {
         SetSnowLevel(1);
 
         // I don't actually have a clue if this does anything in GTAV but hell it doesn't break anything neither so...
-        LoadCloudHat('Snowy 01', 1);
+        //LoadCloudHat('Snowy 01', 1);
 
         SetForcePedFootstepsTracks(true);
         SetForceVehicleTrails(true);
@@ -14,17 +17,41 @@ on('onClientResourceStart', (resource) => {
         RequestNamedPtfxAsset('core_snow');
 
         if (GetConvar('snow_spawnTree', 'true').match(/^("true"|true)$/i)) {
-            const xmasTreeHash = GetHashKey('prop_xmas_ext');
             RequestModel(xmasTreeHash);
 
             const waitInterval = setInterval(() => {
                 if (HasModelLoaded(xmasTreeHash)) {
                     clearInterval(waitInterval);
-                    const treeEntity = CreateObject(xmasTreeHash, ...xmasTreeOrigin, false, false, false);
+                    treeEntity = CreateObject(xmasTreeHash, ...xmasTreeOrigin, false, false, false);
                     FreezeEntityPosition(treeEntity, true);
                     console.log('[snow] xmas tree spawned');
                 }
             }, 1500);
         }
+        console.log('[snow] snow enabled');
+    }
+});
+
+on('onResourceStop', (resource) => {
+    if (resource === 'snow') {
+        ClearOverrideWeather();
+        SetSnowLevel(-1.0);
+
+        // Calling this native seems to crash the script no matter how it's used. It's only used 4 times in the whole game, it could be just buggered.
+        //UnloadCloudHat('Snowy 01', 1); 
+
+        SetForcePedFootstepsTracks(false);
+        SetForceVehicleTrails(false);
+        SetRainLevel(-1.0);
+        RemoveNamedPtfxAsset('core_snow');
+
+        if (GetConvar('snow_spawnTree', 'true').match(/^("true"|true)$/i)) {
+            if (DoesEntityExist(treeEntity)) {
+                DeleteObject(treeEntity);
+                SetModelAsNoLongerNeeded(xmasTreeHash);
+                console.log('[snow] xmas tree removed');
+            }
+        }
+        console.log('[snow] snow disabled');
     }
 });
